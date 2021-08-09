6a2aa7251c542099d29d9307f037d2def376d224
mahru <mahrusferdous@gmail.com>
2021-08-08 22:13:57 -0400

    starter files

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./App.css";
+import TopNav from "./features/components/TopNav";
+import TrailerPreview from "./features/components/TrailerPreview";
+
+function App() {
+    return (
+        <div className="App">
+            <TopNav />
+            <TrailerPreview />
+        </div>
+    );
+}
+
+export default App;
---
+++ src/features/components/TrailerPreview.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function TrailerPreview() {
+    return <div>h</div>;
+}
+
+export default TrailerPreview;
