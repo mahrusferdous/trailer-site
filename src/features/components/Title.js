7c4ec8c8b224817de4c27ecb1a17d888ad72598e
mahru <mahrusferdous@gmail.com>
2021-09-09 22:44:17 -0400

    Title created

--- src/features/components/Title.js
+++ src/features/components/Title.js
@@ -3,197 +3,222 @@ import HoverVideoPlayer from "react-hover-video-player";
 import "./Title.css";
 
 function Title() {
+    function handleClick() {
+        console.log("clicked");
+    }
+
     return (
         <div className="hover-player">
-            <HoverVideoPlayer
-                className="player-wrapper"
-                videoSrc="videos/amazon.mov"
-                style={{
-                    width: "200px",
-                }}
-                videoStyle={{ borderRadius: "10px", overflow: "hidden" }}
-                pausedOverlay={
-                    <img
-                        src="images/amazon.jpg"
-                        alt=""
-                        style={{
-                            width: "200px",
-                            height: "112.500px",
-                            borderRadius: "15px",
-                            padding: "5px",
-                        }}
-                    />
-                }
-                loadingOverlay={<div className="loading-spinner-overlay" />}
-            />
-            <HoverVideoPlayer
-                className="player-wrapper"
-                videoSrc="videos/apple.mov"
-                style={{
-                    width: "200px",
-                }}
-                videoStyle={{ borderRadius: "10px" }}
-                pausedOverlay={
-                    <img
-                        src="images/apple.jpg"
-                        alt=""
-                        style={{
-                            width: "200px",
-                            height: "112.500px",
-                            borderRadius: "15px",
-                            padding: "5px",
-                        }}
-                    />
-                }
-                loadingOverlay={<div className="loading-spinner-overlay" />}
-            />
-            <HoverVideoPlayer
-                className="player-wrapper"
-                videoSrc="videos/disney.mov"
-                style={{
-                    width: "200px",
-                }}
-                videoStyle={{ borderRadius: "10px" }}
-                pausedOverlay={
-                    <img
-                        src="images/disney.jpg"
-                        alt=""
-                        style={{
-                            width: "200px",
-                            height: "112.500px",
-                            borderRadius: "15px",
-                            padding: "5px",
-                        }}
-                    />
-                }
-                loadingOverlay={<div className="loading-spinner-overlay" />}
-            />
-            <HoverVideoPlayer
-                className="player-wrapper"
-                videoSrc="videos/netflix.mov"
-                style={{
-                    width: "200px",
-                }}
-                videoStyle={{ borderRadius: "10px" }}
-                pausedOverlay={
-                    <img
-                        src="images/netflix.jpg"
-                        alt=""
-                        style={{
-                            width: "200px",
-                            height: "112.500px",
-                            borderRadius: "15px",
-                            padding: "5px",
-                        }}
-                    />
-                }
-                loadingOverlay={<div className="loading-spinner-overlay" />}
-            />
-            <HoverVideoPlayer
-                className="player-wrapper"
-                videoSrc="videos/hbo.mov"
-                style={{
-                    width: "200px",
-                }}
-                videoStyle={{ borderRadius: "10px" }}
-                pausedOverlay={
-                    <img
-                        src="images/hbo.jpg"
-                        alt=""
-                        style={{
-                            width: "200px",
-                            height: "112.500px",
-                            borderRadius: "15px",
-                            padding: "5px",
-                        }}
-                    />
-                }
-                loadingOverlay={<div className="loading-spinner-overlay" />}
-            />
-            <HoverVideoPlayer
-                className="player-wrapper"
-                videoSrc="videos/youtube.mov"
-                style={{
-                    width: "200px",
-                }}
-                videoStyle={{ borderRadius: "10px" }}
-                pausedOverlay={
-                    <img
-                        src="images/youtube.jpg"
-                        alt=""
-                        style={{
-                            width: "200px",
-                            height: "112.500px",
-                            borderRadius: "15px",
-                            padding: "5px",
-                        }}
-                    />
-                }
-                loadingOverlay={<div className="loading-spinner-overlay" />}
-            />
-            <HoverVideoPlayer
-                className="player-wrapper"
-                videoSrc="videos/hulu.mov"
-                style={{
-                    width: "200px",
-                }}
-                videoStyle={{ borderRadius: "10px" }}
-                pausedOverlay={
-                    <img
-                        src="images/hulu.jpg"
-                        alt=""
-                        style={{
-                            width: "200px",
-                            height: "112.500px",
-                            borderRadius: "15px",
-                            padding: "5px",
-                        }}
-                    />
-                }
-                loadingOverlay={<div className="loading-spinner-overlay" />}
-            />
-            <HoverVideoPlayer
-                className="player-wrapper"
-                videoSrc="videos/peacock.mov"
-                style={{
-                    width: "200px",
-                }}
-                videoStyle={{ borderRadius: "10px" }}
-                pausedOverlay={
-                    <img
-                        src="images/peacock.jpg"
-                        alt=""
-                        style={{
-                            width: "200px",
-                            height: "112.500px",
-                            borderRadius: "15px",
-                            padding: "5px",
-                        }}
-                    />
-                }
-                loadingOverlay={<div className="loading-spinner-overlay" />}
-            />
-            <HoverVideoPlayer
-                className="player-wrapper"
-                videoSrc="videos/paramount.mov"
-                style={{
-                    width: "200px",
-                }}
-                videoStyle={{ borderRadius: "10px" }}
-                pausedOverlay={
-                    <img
-                        src="images/paramount.jpg"
-                        alt=""
-                        style={{
-                            width: "200px",
-                            height: "112.500px",
-                            borderRadius: "15px",
-                            padding: "5px",
-                        }}
-                    />
-                }
-                loadingOverlay={<div className="loading-spinner-overlay" />}
-            />
+            <button className="hover-player__button" onClick={handleClick}>
+                <HoverVideoPlayer
+                    onClick={() => {
+                        console.log("clicked");
+                    }}
+                    className="player-wrapper"
+                    videoSrc="videos/amazon.mov"
+                    style={{
+                        width: "200px",
+                    }}
+                    videoStyle={{ borderRadius: "10px", overflow: "hidden" }}
+                    pausedOverlay={
+                        <img
+                            src="images/amazon.jpg"
+                            alt=""
+                            style={{
+                                width: "200px",
+                                height: "112.500px",
+                                borderRadius: "15px",
+                                padding: "5px",
+                            }}
+                        />
+                    }
+                    loadingOverlay={<div className="loading-spinner-overlay" />}
+                />
+            </button>
+            <button className="hover-player__button" onClick={handleClick}>
+                <HoverVideoPlayer
+                    className="player-wrapper"
+                    videoSrc="videos/apple.mov"
+                    style={{
+                        width: "200px",
+                    }}
+                    videoStyle={{ borderRadius: "10px" }}
+                    pausedOverlay={
+                        <img
+                            src="images/apple.jpg"
+                            alt=""
+                            style={{
+                                width: "200px",
+                                height: "112.500px",
+                                borderRadius: "15px",
+                                padding: "5px",
+                            }}
+                        />
+                    }
+                    loadingOverlay={<div className="loading-spinner-overlay" />}
+                />
+            </button>
+            <button className="hover-player__button" onClick={handleClick}>
+                <HoverVideoPlayer
+                    className="player-wrapper"
+                    videoSrc="videos/disney.mov"
+                    style={{
+                        width: "200px",
+                    }}
+                    videoStyle={{ borderRadius: "10px" }}
+                    pausedOverlay={
+                        <img
+                            src="images/disney.jpg"
+                            alt=""
+                            style={{
+                                width: "200px",
+                                height: "112.500px",
+                                borderRadius: "15px",
+                                padding: "5px",
+                            }}
+                        />
+                    }
+                    loadingOverlay={<div className="loading-spinner-overlay" />}
+                />
+            </button>
+            <button className="hover-player__button" onClick={handleClick}>
+                <HoverVideoPlayer
+                    className="player-wrapper"
+                    videoSrc="videos/netflix.mov"
+                    style={{
+                        width: "200px",
+                    }}
+                    videoStyle={{ borderRadius: "10px" }}
+                    pausedOverlay={
+                        <img
+                            src="images/netflix.jpg"
+                            alt=""
+                            style={{
+                                width: "200px",
+                                height: "112.500px",
+                                borderRadius: "15px",
+                                padding: "5px",
+                            }}
+                        />
+                    }
+                    loadingOverlay={<div className="loading-spinner-overlay" />}
+                />
+            </button>
+            <button className="hover-player__button" onClick={handleClick}>
+                <HoverVideoPlayer
+                    className="player-wrapper"
+                    videoSrc="videos/hbo.mov"
+                    style={{
+                        width: "200px",
+                    }}
+                    videoStyle={{ borderRadius: "10px" }}
+                    pausedOverlay={
+                        <img
+                            src="images/hbo.jpg"
+                            alt=""
+                            style={{
+                                width: "200px",
+                                height: "112.500px",
+                                borderRadius: "15px",
+                                padding: "5px",
+                            }}
+                        />
+                    }
+                    loadingOverlay={<div className="loading-spinner-overlay" />}
+                />
+            </button>
+            <button className="hover-player__button" onClick={handleClick}>
+                <HoverVideoPlayer
+                    className="player-wrapper"
+                    videoSrc="videos/youtube.mov"
+                    style={{
+                        width: "200px",
+                    }}
+                    videoStyle={{ borderRadius: "10px" }}
+                    pausedOverlay={
+                        <img
+                            src="images/youtube.jpg"
+                            alt=""
+                            style={{
+                                width: "200px",
+                                height: "112.500px",
+                                borderRadius: "15px",
+                                padding: "5px",
+                            }}
+                        />
+                    }
+                    loadingOverlay={<div className="loading-spinner-overlay" />}
+                />
+            </button>
+            <button className="hover-player__button" onClick={handleClick}>
+                <HoverVideoPlayer
+                    className="player-wrapper"
+                    videoSrc="videos/hulu.mov"
+                    style={{
+                        width: "200px",
+                    }}
+                    videoStyle={{ borderRadius: "10px" }}
+                    pausedOverlay={
+                        <img
+                            src="images/hulu.jpg"
+                            alt=""
+                            style={{
+                                width: "200px",
+                                height: "112.500px",
+                                borderRadius: "15px",
+                                padding: "5px",
+                            }}
+                        />
+                    }
+                    loadingOverlay={<div className="loading-spinner-overlay" />}
+                />
+            </button>
+            <button className="hover-player__button" onClick={handleClick}>
+                <HoverVideoPlayer
+                    className="player-wrapper"
+                    videoSrc="videos/peacock.mov"
+                    style={{
+                        width: "200px",
+                    }}
+                    videoStyle={{ borderRadius: "10px" }}
+                    pausedOverlay={
+                        <img
+                            src="images/peacock.jpg"
+                            alt=""
+                            style={{
+                                width: "200px",
+                                height: "112.500px",
+                                borderRadius: "15px",
+                                padding: "5px",
+                            }}
+                        />
+                    }
+                    loadingOverlay={<div className="loading-spinner-overlay" />}
+                />
+            </button>
+            <button className="hover-player__button" onClick={handleClick}>
+                <HoverVideoPlayer
+                    className="player-wrapper"
+                    videoSrc="videos/paramount.mov"
+                    style={{
+                        width: "200px",
+                    }}
+                    videoStyle={{ borderRadius: "10px" }}
+                    pausedOverlay={
+                        <img
+                            src="images/paramount.jpg"
+                            alt=""
+                            style={{
+                                width: "200px",
+                                height: "112.500px",
+                                borderRadius: "15px",
+                                padding: "5px",
+                            }}
+                        />
+                    }
+                    loadingOverlay={<div className="loading-spinner-overlay" />}
+                />
+            </button>
         </div>
     );
 }
